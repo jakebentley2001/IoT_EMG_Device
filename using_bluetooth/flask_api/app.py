06a44e0036cf8edc41580b1b3d16c3acf5bdd921
jakebentley <jakebentley2001@gmail.com>
2024-04-11 12:43:52 -0700

    added the website to the cloud with a dockerfile
    added the website to the cloud with a dockerfile

--- using_bluetooth/flask_api/app.py
+++ using_bluetooth/flask_api/app.py
@@ -2,12 +2,14 @@ from flask import Flask, jsonify, render_template, request
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
+from flask_cors import CORS
 
+load_dotenv()
 
 app = Flask(__name__)
-#load_dotenv()
+CORS(app)
 
-#app.config['DEBUG'] = os.environ.get('FLASK_DEBUG')
+app.config['DEBUG'] = os.environ.get('FLASK_DEBUG')
 
 # Connect to MongoDB
 client = MongoClient('mongodb+srv://jakebentley2001:Sonicpower4@serverlessinstance0.hzqw4sr.mongodb.net/?retryWrites=true&w=majority&appName=ServerlessInstance0')
@@ -37,4 +39,4 @@ def get_recording():
         return jsonify({"error": "Missing record_number parameter"}), 400
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
