6133127fc1cdd36567b87bd3abd75884ed260725
jakebentley <jakebentley2001@gmail.com>
2024-04-10 22:01:06 -0700

    Seperating my python script into receiving and adding data and a flask api
    Seperating my python script into receiving and adding data and a flask api

---
+++ using_bluetooth/data_gather_store.py
@@ -0,0 +1,51 @@
+import asyncio
+import struct
+from bleak import BleakClient
+from pymongo import MongoClient
+import time
+
+# Define the UUIDs of the service and characteristic from Arduino
+address = "705E0B07-DE6F-A403-C7FE-23C804F8A01A"
+SERVICE_UUID = "19B10000-E8F2-537E-4F6C-D104768A1214"
+CHARACTERISTIC_UUID = "19B10001-E8F2-537E-4F6C-D104768A1214"
+
+# Connect to MongoDB
+client = MongoClient('mongodb+srv://jakebentley2001:Sonicpower4@serverlessinstance0.hzqw4sr.mongodb.net/?retryWrites=true&w=majority&appName=ServerlessInstance0')
+db = client['IOT_DEVICE']
+collection = db['muscle_data']
+record = "7"
+
+time_data = []
+sensor_data = []
+
+async def read_data(address):
+    start_time = time.time()
+    
+    async with BleakClient(address) as client:
+        if not client.is_connected:
+            await client.connect()
+        print("Connected!")
+
+        while time.time() - start_time < 10:
+            try:
+                value = await client.read_gatt_char(CHARACTERISTIC_UUID)
+                sensor_val = struct.unpack('<f', value)[0]
+                print("Received data:", sensor_val)
+                
+                time_data.append(len(time_data) + 0.5)
+                sensor_data.append(sensor_val)
+
+            except Exception as e:
+                print("Error reading data:", e)
+
+            await asyncio.sleep(0.05)
+            
+    data_to_insert = {"record_number": record, "data": [time_data, sensor_data]}
+    if data_to_insert:
+        collection.insert_one(data_to_insert)
+
+# Replace 'your_device_address' with the actual address of your Arduino BLE peripheral
+device_address = "705E0B07-DE6F-A403-C7FE-23C804F8A01A"
+
+loop = asyncio.new_event_loop()
+loop.run_until_complete(read_data(device_address))
---
+++ using_bluetooth/flask_api.py
@@ -0,0 +1,34 @@
+from flask import Flask, jsonify, render_template, request
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+# Connect to MongoDB
+client = MongoClient('mongodb+srv://jakebentley2001:Sonicpower4@serverlessinstance0.hzqw4sr.mongodb.net/?retryWrites=true&w=majority&appName=ServerlessInstance0')
+db = client['IOT_DEVICE']
+collection = db['muscle_data']
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/get_recording', methods=['GET'])
+def get_recording():
+    record_number = request.args.get('record_number')
+
+    if record_number:
+        try:
+            document = collection.find_one({"record_number": record_number})
+            if document:
+                sensor_data = document['data'][1]
+                time_data = document['data'][0]
+                return jsonify({"record_number": record_number, "data": sensor_data})
+            else:
+                return jsonify({"error": "Record not found"}), 404
+        except Exception as e:
+            return jsonify({"error": f"Error retrieving record: {str(e)}"}), 500
+    else:
+        return jsonify({"error": "Missing record_number parameter"}), 400
+
+if __name__ == "__main__":
+    app.run(debug=True)
